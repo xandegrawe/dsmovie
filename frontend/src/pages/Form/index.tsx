452493fb473d5929769195f4a2adff9e6f1e9c7a
xandegrawe <xandegrawe@gmail.com>
2022-01-15 22:56:06 -0300

    show score

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,12 +1,11 @@
 import FormCard from "components/FormCard";
 import { Link, useParams } from "react-router-dom";
-import { Movie } from "types/movie";
 
 
 
 function Form() {
 
-const params = useParams();
+    const params = useParams();
 
     return (
         <FormCard movieId={`${params.movieId}`} />
